8f51c4ae93519398b1785d28fe039ae787520d4a
emmad276 <emmadahmed90@gmail.com>
2024-03-22 12:25:18 -0700

    add all files

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) Computer will generate a random number
+// 2) User input for guessing game
+// 3) Campare user input with computer generated number and show result
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    { name: "userguessnumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6:", },
+]);
+if (answer.userguessnumber === randomnumber) {
+    console.log("Congratulations! you gussed an absolutely right number");
+}
+else {
+    console.log("Please try again, you guessed wrong number");
+}
